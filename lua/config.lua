cd3a6132026101c6bdfd8b7cb0b9fe670f010df7
Thai Duc Dung <mducdung255@gmail.com>
2021-04-25 23:37:06 +0700

    add vim config

---
+++ lua/config.lua
@@ -0,0 +1,17 @@
+vim.g.mapleader = ' '
+vim.g.syntax = 'on'
+vim.g.encoding = 'utf-8'
+
+vim.g.tabstop = 3
+vim.g.softtabstop = 3
+vim.g.shiftwidth = 3
+vim.g.expandtab = true
+
+vim.g.updatetime = 200
+vim.g.smartindent = true
+vim.g.autoindent = true
+vim.g.copyindent = true
+vim.g.ignorecase = true
+vim.g.smartcase = true
+vim.g.noswapfile = true
+vim.g.relative_number = true
