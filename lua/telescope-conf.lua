6cd84d967ed5fe8c9cdfa7e4e5c63ef79248f063
Thai Duc Dung <mducdung255@gmail.com>
2021-07-16 10:33:41 +0700

    remove telescope deprecated config

--- lua/telescope-conf.lua
+++ lua/telescope-conf.lua
@@ -5,8 +5,6 @@ local actions = require('telescope.actions')
 require('telescope').setup {
    defaults = {
       find_command         = { 'rg', '--ignore', '--hidden', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', },
-      -- prompt_position      = 'bottom',
-      -- prompt_prefix = ' ',
       prompt_prefix        = ' ',
       selection_caret      = ' ',
       entry_prefix         = '  ',
@@ -14,16 +12,11 @@ require('telescope').setup {
       selection_strategy   = 'reset',
       sorting_strategy     = 'descending',
       layout_strategy      = 'horizontal',
-      layout_defaults      = { horizontal = { mirror = false }, vertical = { mirror = false } },
       file_sorter          = require'telescope.sorters'.get_fuzzy_file,
       file_ignore_patterns = { '.git', 'node_modules', '.package-lock.json' },
       generic_sorter       = require'telescope.sorters'.get_generic_fuzzy_sorter,
       path_display         = { 'absolute' },
       winblend             = 0,
-      -- width             = 0.75,
-      -- preview_cutoff    = 120,
-      -- results_height    = 1,
-      -- results_width     = 0.8,
       border            = {},
       borderchars       = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
       color_devicons    = true,
@@ -43,7 +36,7 @@ require('telescope').setup {
             -- To disable a keymap, put [map] = false
             -- So, to not map '<C-n>', just put
             -- ['<c-x>'] = false,
-            ['<esc>'] = actions.close,
+            ['<ESC>'] = actions.close,
 
             -- Otherwise, just set the mapping to the function that you want it to be.
             -- ['<C-i>'] = actions.select_horizontal,
@@ -58,7 +51,6 @@ require('telescope').setup {
             ['<C-j>'] = actions.move_selection_next,
             ['<C-k>'] = actions.move_selection_previous,
             ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
-            -- ['<C-i>'] = my_cool_custom_action,
          }
       }
    }
