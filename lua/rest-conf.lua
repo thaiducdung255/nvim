22a8a08ea3b7b7e849fe4b3c694a279c0064a022
Thai Duc Dung <mducdung255@gmail.com>
2021-08-04 22:46:29 +0700

    update ts snippet

--- lua/rest-conf.lua
+++ lua/rest-conf.lua
@@ -1,3 +1,4 @@
 Nmap('<LEADER><LEADER>', ':lua require(\'rest-nvim\').run()<CR>')
 Nmap('<LEADER><CR>', ':lua require(\'rest-nvim\').run(true)<CR>')
 vim.cmd('autocmd BufEnter,BufNew *.http set ft=http')
+vim.cmd('autocmd FileType httpResult set sw=2')
