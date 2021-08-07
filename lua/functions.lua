1f36780519f3e400f6511e848c57ddaff30a8c51
Thai Duc Dung <mducdung255@gmail.com>
2021-08-08 03:35:43 +0700

    disable functions.lua

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,3 +1,41 @@
-print('hello world')
+function _G.customMotions(motionKey, targetKey, repeatCount)
+   local targets = {
+      j    = '(',
+      J    = ')',
+      k    = '[',
+      K    = ']',
+      l    = '{',
+      L    = '}',
+      h    = '<',
+      H    = '>',
+      i    = '\'',
+      I    = '"',
+      o   = '`',
+   }
 
-Nmap('no', [[:lua require('functions')<cr>]])
+   local motions = {
+      g    = 'f',
+      s    = 'v',
+      d    = 'd',
+      c    = 'c',
+      z    = 'c',
+   }
+
+   local prevY = vim.api.nvim_win_get_cursor(0)[2]
+
+   while (repeatCount > 0) do
+      local cmd = ':normal ' .. motions[motionKey] .. targets[targetKey]
+      vim.cmd(cmd)
+      repeatCount = repeatCount - 1
+   end
+
+   local currentY = vim.api.nvim_win_get_cursor(0)[2]
+
+   if prevY == currentY then
+      
+   end
+
+   print('hello world: ', prevY, currentY)
+end
+
+Nmap('nn', [[:lua customMotions('g', 'j', 1)<CR>]])
