8bc0496e47548b9b90bc803b1b23c167c73b690b
Thai Duc Dung <mducdung255@gmail.com>
2021-08-09 21:21:07 +0700

    update icon for some special files

---
+++ lua/devicon-conf.lua
@@ -0,0 +1,110 @@
+require'nvim-web-devicons'.setup {
+   override = {
+      zsh = {
+         icon = '',
+         color = '#428850',
+         name = 'Zsh'
+      },
+      http = {
+         icon = '黎',
+         color = '#428850',
+         name = 'Http'
+      },
+      ['env.sample'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.env.sample'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['package.json'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['package-lock.json'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['yarn.lock'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.env'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['tsconfig.tsbuildinfo'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['tsconfig.json'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.editorconfig'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.eslintrc'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.prettierignore'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['.prettierrc'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['readme.md'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['index.js'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['index.ts'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['main.js'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['main.ts'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['app.js'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+      ['app.ts'] = {
+         icon = '',
+         color = '#428850',
+         name = 'Env'
+      },
+   };
+   default = true;
+}
