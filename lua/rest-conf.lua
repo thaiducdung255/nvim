73ba5ec720da959f198482a91745e2a49d3ff39d
Thai Duc Dung <mducdung255@gmail.com>
2021-08-19 01:25:05 +0700

    add new filetype: http

--- lua/rest-conf.lua
+++ lua/rest-conf.lua
@@ -1,4 +1,3 @@
-Nmap('<LEADER><CR>', ':lua require(\'rest-nvim\').run()<CR>')
-Nmap('<LEADER><TAB>', ':lua require(\'rest-nvim\').run(true)<CR>')
-vim.cmd('autocmd BufEnter,BufNew *.http set ft=http')
-vim.cmd('autocmd FileType httpResult set sw=2')
+Nmap('<LEADER><CR>', [[:lua require('rest-nvim').run()<CR>]])
+Nmap('<LEADER><TAB>', [[:lua require(\'rest-nvim\').run(true)<CR>]])
+vim.cmd([[autocmd FileType httpResult lua vim.bo.sw=2]])
