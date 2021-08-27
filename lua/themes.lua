3e722ca3460862169d2693df1f69f758dd479ac4
DungTD10 <mducdung255@gmail.com>
2021-08-28 01:43:11 +0700

    removed unused themes configs

--- lua/themes.lua
+++ lua/themes.lua
@@ -3,19 +3,6 @@ vim.g.edge_enable_italic = 1
 vim.g.edge_disable_italic_comment = 1
 vim.cmd('colorscheme edge')
 
--- vim.g.codedark_conservative = 0
--- vim.cmd[[colorscheme codedark]]
-
--- vim.g.material_style = 'darker'
--- vim.g.material_italic_comments = true
--- vim.g.material_italic_keywords = true
--- vim.g.material_italic_functions = true
--- vim.g.material_italic_variables = true
--- vim.g.material_contrast = true
--- vim.g.material_borders = true
--- vim.g.material_disable_background = false
--- vim.cmd[[colorscheme material]]
-
 vim.cmd[[function! SynStack()
   if !exists("*synstack")
     return
@@ -27,9 +14,9 @@ nnoremap si :call SynStack()<CR>
 
 -- custom highlight
 -- highlight .http files
-vim.cmd('hi httpComment gui=italic guifg=#7f8490')
-vim.cmd('hi httpMethod gui=bold guifg=#cc6600')
-vim.cmd('hi httpPath guifg=#00cc66')
+-- vim.cmd('hi httpComment gui=italic guifg=#7f8490')
+-- vim.cmd('hi httpMethod gui=bold guifg=#cc6600')
+-- vim.cmd('hi httpPath guifg=#00cc66')
 
 -- custom highlight colorscheme
 vim.cmd('hi TabLineSel guibg=grey')
