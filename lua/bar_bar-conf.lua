3822f87ba0805ba6e98237b44268b6032d5366b0
DungTD10 <mducdung255@gmail.com>
2021-09-06 01:28:33 +0700

    config barbar

--- lua/bar_bar-conf.lua
+++ lua/bar_bar-conf.lua
@@ -1,24 +1,16 @@
--- Move to previous/next
--- Goto buffer in position...
-Nmap('<LEADER><LEADER>q', ':BufferGoto 1<CR>')
-Nmap('<LEADER><LEADER>w', ':BufferGoto 2<CR>')
-Nmap('<LEADER><LEADER>e', ':BufferGoto 3<CR>')
-Nmap('<LEADER><LEADER>r', ':BufferGoto 4<CR>')
-Nmap('<LEADER><LEADER>t', ':BufferGoto 5<CR>')
-Nmap('<LEADER><LEADER>a', ':BufferGoto 6<CR>')
-Nmap('<LEADER><LEADER>s', ':BufferGoto 7<CR>')
-Nmap('<LEADER><LEADER>d', ':BufferGoto 8<CR>')
-Nmap('<LEADER><LEADER>f', ':BufferGoto 9<CR>')
-Nmap('<LEADER><LEADER>g', ':BufferGoto 10<CR>')
--- Wipeout buffer
---                          :BufferWipeout<CR>
--- Close commands
---                          :BufferCloseAllButCurrent<CR>
---                          :BufferCloseBuffersLeft<CR>
---                          :BufferCloseBuffersRight<CR>
--- Magic buffer-picking mode
-Nmap('<C-s>', ':BufferPick<CR>')
+Nmap('<Tab>', ':BufferPick<CR>')
+Nmap('<LEADER>bs', ':BufferOrderByBufferNumber<CR>')
 
--- Sort automatically by...
-Nmap('<LEADER>bd', ':BufferOrderByDirectory<CR>')
-Nmap('<LEADER>bl', ':BufferOrderByLanguage<CR>')
+vim.g.bufferline = {
+   animation        = true,
+   auto_hide        = false,
+   tabpages         = true,
+   closable         = false,
+   clickable        = false,
+   icons            = true,
+   insert_at_end    = true,
+   maximum_padding  = 1,
+   maximum_length   = 20,
+   semantic_letters = true,
+   no_name_title    = '<unnamed>',
+}
