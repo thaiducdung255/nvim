652b4eb38cc76c94d4fd0fcaf1186cc5be0d2bd3
DungTD10 <mducdung255@gmail.com>
2021-09-06 08:43:47 +0700

    change buffer tab highlight

--- lua/bar_bar-conf.lua
+++ lua/bar_bar-conf.lua
@@ -2,15 +2,26 @@ Nmap('<Tab>', ':BufferPick<CR>')
 Nmap('<LEADER>bs', ':BufferOrderByBufferNumber<CR>')
 
 vim.g.bufferline = {
-   animation        = true,
+   animation        = false,
    auto_hide        = false,
    tabpages         = true,
    closable         = false,
    clickable        = false,
-   icons            = true,
+   icons            = false,
    insert_at_end    = true,
    maximum_padding  = 1,
    maximum_length   = 20,
    semantic_letters = true,
    no_name_title    = '<unnamed>',
 }
+
+vim.cmd([[hi BufferCurrent guibg=#002966]])
+vim.cmd([[hi BufferCurrentSign guibg=#002966]])
+vim.cmd([[hi BufferCurrentMod guibg=#002966 gui=bold,italic guifg=none]])
+
+vim.cmd([[hi BufferVisible guibg=#808080]])
+vim.cmd([[hi BufferVisibleSign guibg=#808080]])
+vim.cmd([[hi BufferVisibleMod guibg=#808080 gui=bold,italic guifg=#2c2e34]])
+
+vim.cmd([[hi BufferInactiveMod gui=bold,italic]])
+vim.cmd([[hi BufferInactive gui=none]])
