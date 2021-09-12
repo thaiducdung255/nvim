7a19c0ea5fd3689c13a46f295c4ec4904ffde038
DungTD10 <mducdung255@gmail.com>
2021-09-12 20:00:09 +0700

    update custom telescope picker: eslint diagnostic -  show detail diagnostic for each file

--- lua/telescope_custom-conf.lua
+++ lua/telescope_custom-conf.lua
@@ -8,12 +8,10 @@ local E = {}
 local function get_delta_previewer(action)
    return previewers.new_termopen_previewer {
       get_command = function(entry)
-      -- this is for status
       if action == 'diff' then
          return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=true', 'diff', entry.value }
       end
 
-      -- this is for git diff, git (b)commits
       return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=true', 'diff', entry.value .. '^!' }
 
       end
@@ -60,43 +58,51 @@ E.delta_git_status = function(opts)
    builtin.git_status(opts)
 end
 
-local function splitFileDiagnostic(str, delimiter)
-   local result = {};
+local function reformat_eslint_diagnostics(str, delimiter)
+   local filenames = {}
+   local line_diagnostics = {}
+   local index = 1
 
-   for match in (str):gmatch("(.-)"..delimiter) do
-      local pre = match:sub(0, 1)
+   for diagnostic_record in (str):gmatch("(.-)"..delimiter) do
+      local pre = diagnostic_record:sub(0, 1)
 
       if pre == '\n' or pre == '/' then
          if (pre == '\n') then
-            match = match:sub(2)
+            diagnostic_record = diagnostic_record:sub(2)
          end
 
-         local file_name = match:match('^[a-zA-Z0-9\\s/%.%-_]+'):sub(vim.fn.getcwd():len()+2)
-         local file_diagnostics = match:match('\n.*'):sub(2)
-         table.insert(result, { file_name, file_diagnostics })
+         local file_name = diagnostic_record:match('^[a-zA-Z0-9\\s/%.%-_]+'):sub(vim.fn.getcwd():len()+2)
+         local file_diagnostics = diagnostic_record:match('\n.*'):sub(2)
+         table.insert(filenames, file_name)
+         line_diagnostics[index] = file_diagnostics
+         index = index + 1
       end
    end
 
-   return result;
+   return filenames, line_diagnostics
 end
 
-E.eslint_diagnostics = function(otps)
+E.eslint_diagnostics = function()
    local raw_eslint_diagnostics = vim.fn.system('eslint .')
-   local eslint_diagnostics = splitFileDiagnostic(raw_eslint_diagnostics, '\n\n')
+   local filenames, lines = reformat_eslint_diagnostics(raw_eslint_diagnostics, '\n\n')
 
-   opts = otps or {}
+   local otps = {
+      layout_config = {
+         preview_width = 0.76
+      }
+   }
 
    pickers.new(otps, {
-      prompt_title = 'eslint diagnostics',
-      finder = finders.new_table{
-         results = eslint_diagnostics,
-         entry_maker = function(entry)
-            return {
-               value = entry,
-               display = entry[1],
-               ordinal = entry[1],
-            }
+      prompt_title = 'Eslint diagnostics',
+      previewer = previewers.new_termopen_previewer{
+         get_command = function(entry)
+            local index = entry.index
+
+            return { "echo", lines[index] }
          end
+      },
+      finder = finders.new_table{
+         results = filenames,
       },
       sorter = sorters.fuzzy_with_index_bias(),
    }):find(otps)
