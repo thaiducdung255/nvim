a1e6582d183f27cdf22c5da2f16092983086ede7
DungTD10 <mducdung255@gmail.com>
2021-09-16 03:03:40 +0700

    add ts for .http ft

--- lua/rest-conf.lua
+++ lua/rest-conf.lua
@@ -1,3 +1,12 @@
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+parser_configs.http = {
+   install_info = {
+      url = "https://github.com/NTBBloodbath/tree-sitter-http",
+      files = { "src/parser.c" },
+      branch = "main",
+   },
+}
+
 Nmap('<LEADER><CR>', [[:lua require('rest-nvim').run()<CR>]])
 Nmap('<LEADER><TAB>', [[:lua require('rest-nvim').run(true)<CR>]])
 vim.cmd([[autocmd FileType httpResult lua vim.bo.sw=2]])
