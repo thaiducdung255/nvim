d618daaf5f721b2e6db6c7ea73cbeba892be73e7
DungTD10 <mducdung255@gmail.com>
2021-10-03 23:13:03 +0700

    format config source

--- lua/nvim_autopairs-conf.lua
+++ lua/nvim_autopairs-conf.lua
@@ -3,7 +3,7 @@ require('nvim-autopairs').setup({
    ignored_next_char         = '[%w%.]'
 })
 
-require("nvim-autopairs.completion.cmp").setup({
+require('nvim-autopairs.completion.cmp').setup({
    map_cr       = true, --  map <CR> on insert mode
    map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
    auto_select  = true, -- automatically select the first item
