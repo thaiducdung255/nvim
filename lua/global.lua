0578b6f287b7ab167641b7f3cde378c32a8fed03
DungTD10 <mducdung255@gmail.com>
2021-10-03 21:12:27 +0700

    update mapping fn, fix silent opts mapping make mapping not work as expected

--- lua/global.lua
+++ lua/global.lua
@@ -1,61 +1,61 @@
-local function map(lhs, rhs, mode, opts)
+local function map(lhs, rhs, mode, isSilentOff)
    local options = { noremap = true, silent = true }
-   local vimMode = mode
-   if opts then options = vim.tbl_extend('force', options, opts) end
-   if mode == nil then vimMode = 'n' end
+   if  isSilentOff == true then options.silent = false end
+   local vimMode = 'n'
+   if mode then vimMode = mode end
    vim.api.nvim_set_keymap(vimMode, lhs, rhs, options)
 end
 
-function Nmap(lhs, rhs, opts)
-   map(lhs, rhs, 'n', opts)
+function Nmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'n', isSilentOff)
 end
 
-function Cmap(lhs, rhs, opts)
-   map(lhs, rhs, 'c', opts)
+function Cmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'c', isSilentOff)
 end
 
-function Imap(lhs, rhs, opts)
-   map(lhs, rhs, 'i', opts)
+function Imap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'i', isSilentOff)
 end
 
-function Tmap(lhs, rhs, opts)
-   map(lhs, rhs, 't', opts)
+function Tmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 't', isSilentOff)
 end
 
-function Vmap(lhs, rhs, opts)
-   map(lhs, rhs, 'v', opts)
+function Vmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'v', isSilentOff)
 end
 
-function Icmap(lhs, rhs, opts)
-   map(lhs, rhs, 'i', opts)
-   map(lhs, rhs, 'c', opts)
+function Icmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'i', isSilentOff)
+   map(lhs, rhs, 'c', isSilentOff)
 end
 
-function Map(lhs, rhs, opts)
-   map(lhs, rhs, 'i', opts)
-   map(lhs, rhs, 'n', opts)
-   map(lhs, rhs, 'v', opts)
-   map(lhs, rhs, 'c', opts)
+function Map(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'i', isSilentOff)
+   map(lhs, rhs, 'n', isSilentOff)
+   map(lhs, rhs, 'v', isSilentOff)
+   map(lhs, rhs, 'c', isSilentOff)
 end
 
-function Inmap(lhs, rhs, opts)
-   map(lhs, rhs, 'i', opts)
-   map(lhs, rhs, 'n', opts)
+function Inmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'i', isSilentOff)
+   map(lhs, rhs, 'n', isSilentOff)
 end
 
-function Nvmap(lhs, rhs, opts)
-   map(lhs, rhs, 'n', opts)
-   map(lhs, rhs, 'v', opts)
+function Nvmap(lhs, rhs, isSilentOff)
+   map(lhs, rhs, 'n', isSilentOff)
+   map(lhs, rhs, 'v', isSilentOff)
 end
 
-function Invmap(lhs, rhs, opts)
-   Inmap(lhs, rhs, opts)
-   Vmap(lhs, rhs, opts)
+function Invmap(lhs, rhs, isSilentOff)
+   Inmap(lhs, rhs, isSilentOff)
+   Vmap(lhs, rhs, isSilentOff)
 end
 
-function Map(lhs, rhs, opts)
-   Invmap(lhs, rhs, opts)
-   Cmap(lhs, rhs, opts)
+function Map(lhs, rhs, isSilentOff)
+   Invmap(lhs, rhs, isSilentOff)
+   Cmap(lhs, rhs, isSilentOff)
 end
 
 DATA_PATH   = vim.fn.stdpath('data')
