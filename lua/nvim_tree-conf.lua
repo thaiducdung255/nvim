51cedd9b354b2f7c107fa3d4258cad6809a6872e
DungTD10 <mducdung255@gmail.com>
2021-10-11 09:27:25 +0700

    remove deprecated configs for nvim-tree

--- lua/nvim_tree-conf.lua
+++ lua/nvim_tree-conf.lua
@@ -65,7 +65,7 @@ require'nvim-tree'.setup {
    open_on_tab         = false,
    hijack_cursor       = false,
    update_cwd          = true,
-   lsp_diagnostics     = true,
+   -- lsp_diagnostics     = true,
    update_focused_file = {
       enable      = false,
       update_cwd  = false,
