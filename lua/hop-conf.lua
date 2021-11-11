3900cdb0327b1dce93ad46d21ec1511c969579f2
DungTD10 <mducdung255@gmail.com>
2021-11-11 14:47:03 +0700

    config hop

--- lua/hop-conf.lua
+++ lua/hop-conf.lua
@@ -1,3 +1,9 @@
+local hop = require('hop')
+
+hop.setup({
+   keys = 'asdfghjkl;zxcvnm,.qweruiop'
+})
+
 Nmap('<Leader>s',        ':HopChar1<CR>')
 Nmap('<Leader>w',        ':HopWord<CR>')
 Nmap('<Leader>d',        ':HopChar2<CR>')
