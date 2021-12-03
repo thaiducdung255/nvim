ceed680994d04458bd78aa5da940520d699ade02
DungTD10 <mducdung255@gmail.com>
2021-12-03 11:36:05 +0700

    config rest.nvim: fix highlight conflict http syntax and treesitter http highlight

--- lua/rest-conf.lua
+++ lua/rest-conf.lua
@@ -1,14 +1,22 @@
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-parser_configs.http = {
-   install_info = {
-      url    = 'https://github.com/NTBBloodbath/tree-sitter-http',
-      files  = { 'src/parser.c' },
-      branch = 'main',
+require('rest-nvim').setup({
+   result_split_horizontal = false,
+   skip_ssl_verifycation = true,
+   highlight = {
+      enabled = false,
+      timeout = 150,
    },
-}
+   result = {
+      show_url = true,
+      show_http_info = true,
+      show_header = false,
+   },
+   jump_to_request = true,
+   env_file = '.env',
+   custom_dynamic_variables = {},
+})
 
 Nmap('<LEADER><CR>', [[:lua require('rest-nvim').run()<CR>]])
 Nmap('<LEADER><TAB>', [[:lua require('rest-nvim').run(true)<CR>]])
 
 vim.cmd([[autocmd FileType httpResult,tsplayground lua vim.bo.sw=2]])
+vim.cmd([[autocmd FileType httpResult,http TSDisableAll highlight]])
