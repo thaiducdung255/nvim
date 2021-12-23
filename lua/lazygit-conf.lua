40358176aef90e1db4333c65273660f3eb379be0
DungTD10 <mducdung255@gmail.com>
2021-12-24 00:51:58 +0700

    update lazygit ui

--- lua/lazygit-conf.lua
+++ lua/lazygit-conf.lua
@@ -1,7 +1,7 @@
 vim.g.lazygit_floating_window_winblend       = 0
 vim.g.lazygit_floating_window_scaling_factor = 0.92
 vim.g.lazygit_floating_window_corner_chars   = {'╭', '╮', '╰', '╯'}
-vim.g.lazygit_floating_window_use_plenary    = 0
+vim.g.lazygit_floating_window_use_plenary    = 1
 vim.g.lazygit_use_neovim_remote              = 1
 
 Nmap('<LEADER>gg', ':LazyGit<CR>')
