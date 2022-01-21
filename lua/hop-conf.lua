375fdb09b472825a5e9b501499e491f613619caf
DungTD10 <mducdung255@gmail.com>
2022-01-21 22:40:18 +0700

    update hop shortcut

--- lua/hop-conf.lua
+++ lua/hop-conf.lua
@@ -1,6 +1,7 @@
 require('hop').setup({})
 
-Nmap('<Leader>s',        ':HopChar1<CR>')
+Nmap('f',                ':HopChar1CurrentLine<CR>')
+Nmap('s',                ':HopChar2<CR>')
 Nmap('<Leader>w',        ':HopWord<CR>')
 Nmap('<Leader>d',        ':HopChar2<CR>')
 Nmap('<Leader><Esc>',    ':HopLine<CR>')
