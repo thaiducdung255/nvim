dae09ff08d01894855c507f2d187f67e9a017bcf
DungTD10 <mducdung255@gmail.com>
2022-04-21 21:32:16 +0700

    fix cmp cannot nav items in command mode

--- lua/cmp-conf.lua
+++ lua/cmp-conf.lua
@@ -4,12 +4,13 @@ vim.opt.completeopt = 'menuone,noselect'
 local lspkind = require('lspkind')
 
 local source_mapping = {
-   buffer          = '[Buf]',
-   nvim_lsp        = '[Lsp]',
-   nvim_lua        = '[Lua]',
-   vsnip           = '[Snp]',
-   cmdline         = '[Cmd]',
-   cmdline_history = '[His]',
+   buffer                  = '[Buf]',
+   nvim_lsp                = '[Lsp]',
+   nvim_lua                = '[Lua]',
+   vsnip                   = '[Snp]',
+   cmdline                 = '[Cmd]',
+   cmdline_history         = '[His]',
+   nvim_lsp_signature_help = '[Arg]',
 }
 
 cmp.setup {
@@ -32,8 +33,10 @@ cmp.setup {
       },
    },
    mapping = {
-      ['<Tab>']   = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ['<Tab>']   = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+      ['<C-k>']   = cmp.mapping.scroll_docs(-4),
+      ['<C-j>']   = cmp.mapping.scroll_docs(4),
       ['<CR>']    = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
    },
    formatting = {
@@ -41,9 +44,7 @@ cmp.setup {
 			vim_item.kind = lspkind.presets.default[vim_item.kind]
 			local menu = source_mapping[entry.source.name]
 
-			if entry.source.name == 'cmp_tabnine' then
-				vim_item.kind = ''
-         elseif entry.source.name == 'cmdline' or entry.source.name == 'cmdline_history' then
+         if entry.source.name == 'cmdline' or entry.source.name == 'cmdline_history' then
             vim_item.dup = 0
          end
 
