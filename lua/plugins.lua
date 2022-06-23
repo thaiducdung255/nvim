1153d84ed8eae4930569a0bcf40d49384f77bbb7
DungTD10 <mducdung255@gmail.com>
2022-06-23 23:58:01 +0700

    add distant.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -82,6 +82,21 @@ return require('packer').startup({
       -- debugger
       use 'mfussenegger/nvim-dap'
 
+      -- ssh
+      use {
+         'chipsenkbeil/distant.nvim',
+         config = function()
+            require('distant').setup {
+               -- Applies Chip's personal settings to every machine you connect to
+               --
+               -- 1. Ensures that distant servers terminate with no connections
+               -- 2. Provides navigation bindings for remote directories
+               -- 3. Provides keybinding to jump into a remote file's parent directory
+               ['*'] = require('distant.settings').chip_default()
+            }
+         end
+      }
+
       -- db management
       use 'tpope/vim-dadbod'
       use 'kristijanhusak/vim-dadbod-ui'
