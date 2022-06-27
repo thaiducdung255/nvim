1e49dbf04c06dec851725e0c5940601ee9c74e20
DungTD10 <mducdung255@gmail.com>
2022-06-27 14:10:25 +0700

    update err: custom highlights not applied

--- lua/themes.lua
+++ lua/themes.lua
@@ -10,3 +10,24 @@ vim.cmd [[function! SynStack()
 ]]
 
 require('themes/' .. vim.g.theme)
+
+-- custom highlight colorscheme
+-- vim.cmd('hi TabLineSel guibg=grey')
+vim.cmd('hi link TSConstructor TSVariable')
+
+-- custom LSP highlight
+vim.cmd('hi LspDiagnosticsDefaultError gui=None guifg=Red')
+vim.cmd('hi LspDiagnosticsDefaultWarning gui=None guifg=Orange')
+vim.cmd('hi LspDiagnosticsDefaultHint gui=None')
+
+-- custom LSP virtual text highlight
+vim.cmd('hi VirtualTextError gui=None guifg=Red')
+vim.cmd('hi VirtualTextWarning gui=None guifg=Orange')
+vim.cmd('hi VirtualTextInfo gui=italic guifg=#dfe4ed')
+vim.cmd('hi VirtualTextHint gui=None guifg=#dfe4ed')
+
+-- tsserver custom highlight
+vim.cmd('hi TSBoolean guifg=#deb974')
+
+-- cursor line highlight
+vim.cmd('hi CursorLine guibg=#2c2e42')
--- lua/themes/edge.lua
+++ lua/themes/edge.lua
@@ -5,26 +5,5 @@ vim.g.edge_disable_italic_comment  = 1
 vim.g.edge_better_performance      = 1
 vim.g.edge_disable_terminal_colors = 1
 
--- custom highlight colorscheme
--- vim.cmd('hi TabLineSel guibg=grey')
-vim.cmd('hi link TSConstructor TSVariable')
-
--- custom LSP highlight
-vim.cmd('hi LspDiagnosticsDefaultError gui=None guifg=Red')
-vim.cmd('hi LspDiagnosticsDefaultWarning gui=None guifg=Orange')
-vim.cmd('hi LspDiagnosticsDefaultHint gui=None')
-
--- custom LSP virtual text highlight
-vim.cmd('hi VirtualTextError gui=None guifg=Red')
-vim.cmd('hi VirtualTextWarning gui=None guifg=Orange')
-vim.cmd('hi VirtualTextInfo gui=italic guifg=#dfe4ed')
-vim.cmd('hi VirtualTextHint gui=None guifg=#dfe4ed')
-
--- tsserver custom highlight
-vim.cmd('hi TSBoolean guifg=#deb974')
-
 vim.cmd [[let g:edge_colors_override = { 'black': ['#333333', '232'], 'bg0': ['#202120', '235'], 'bg2': ['#1e1e1e', '236'] }]]
 vim.cmd('colorscheme edge')
-
--- cursor line highlight
-vim.cmd('hi CursorLine guibg=#2c2e42')
