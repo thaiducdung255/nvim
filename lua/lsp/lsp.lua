5219eddc65807bc35b674635b2451a980acb5f3b
DungTD10 <mducdung255@gmail.com>
2022-08-09 16:12:50 +0700

    config yamlls

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -103,6 +103,18 @@ lspconfig.jsonls.setup {
    }
 }
 
+lspconfig.yamlls.setup {
+   settings = {
+      yaml = {
+         schemas = {
+            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            ["../path/relative/to/file.yml"] = "/.github/workflows/*",
+            ["/path/from/root/of/project"] = "/.github/workflows/*"
+         },
+      },
+   }
+}
+
 lspconfig.dockerls.setup {
    on_attach = function(client, bufnr)
       set_lsp_config(client, bufnr)
