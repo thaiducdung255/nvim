efc810a6e1025eb4e088cbde64dc3cfbbfce958c
DungTD10 <mducdung255@gmail.com>
2022-10-11 09:49:20 +0700

    style: remove unused configs

--- lua/_themes.lua
+++ lua/_themes.lua
@@ -12,7 +12,6 @@ vim.cmd [[function! SynStack()
 ]]
 
 -- custom highlight colorscheme
--- vim.cmd('hi TabLineSel guibg=grey')
 vim.cmd('hi link TSConstructor TSVariable')
 
 -- custom LSP highlight
@@ -26,10 +25,6 @@ vim.cmd('hi VirtualTextWarning gui=None guifg=Orange')
 vim.cmd('hi Visual gui=None guibg=#838383')
 vim.cmd('hi VirtualTextInfo gui=italic guifg=#dfe4ed')
 vim.cmd('hi VirtualTextHint gui=None guifg=#dfe4ed')
-
--- vim.cmd('hi TabLine gui=None guibg=Red guifg=Red ctermbg=Red ctermfg=Red')
--- vim.cmd('hi TabLineFill gui=None guibg=Blue guifg=Blue ctermbg=Blue ctermfg=Blue')
--- vim.cmd('hi TabLineSel gui=None guibg=Green guifg=Green ctermbg=Green ctermfg=Green')
 
 -- tsserver custom highlight
 vim.cmd('hi TSBoolean guifg=#deb974')
