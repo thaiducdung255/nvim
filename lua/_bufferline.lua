9cf746ca98fbbdc194366f397fcf831619cb356c
DungTD10 <mducdung255@gmail.com>
2022-12-06 11:51:15 +0700

    feat: hide tab indicator

--- lua/_bufferline.lua
+++ lua/_bufferline.lua
@@ -56,7 +56,7 @@ require('bufferline').setup {
       show_buffer_icons       = true,
       show_buffer_close_icons = false,
       show_close_icon         = false,
-      show_tab_indicators     = true,
+      show_tab_indicators     = false,
       persist_buffer_sort     = true,
       separator_style         = 'slant',
       enforce_regular_tabs    = false,
