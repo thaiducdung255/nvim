1f4fd67d3e98e4107626b12670adda4a3d365f99
DungTD10 <mducdung255@gmail.com>
2022-12-19 16:07:52 +0700

    ci: change dadbod execute query cmd from v mode to n mode

--- lua/_vim_dadbod.lua
+++ lua/_vim_dadbod.lua
@@ -2,7 +2,7 @@ vim.g.db_ui_show_help          = 0
 vim.g.db_ui_use_nerd_fonts     = 1
 vim.g.db_ui_show_database_icon = 1
 
-vim.cmd([[vmap zz <Plug>(DBUI_ExecuteQuery)]])
+vim.cmd([[nmap zz <Plug>(DBUI_ExecuteQuery)]])
 vim.cmd([[nmap sf <Plug>(DBUI_SaveQuery)]])
 
 vim.cmd([[au BufEnter [0-9].json set ft=mongodoc]])
