0f76aa5859d467660b58aa9a8c2593d375796a04
DungTD4 <dungtd4@cloudverse.ai>
2023-06-30 13:54:35 +0700

    feat: update indent config

--- lua/_indent_blank_line.lua
+++ lua/_indent_blank_line.lua
@@ -7,5 +7,5 @@ vim.g.indent_blankline_use_treesitter                 = true
 vim.g.indent_blankline_show_current_context           = true
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_char                           = '¦'
-vim.g.indent_blankline_context_char                   = '¦'
-vim.cmd('hi IndentBlanklineContextChar cterm=nocombine ctermfg=246 gui=nocombine guifg=Red')
+vim.g.indent_blankline_context_char                   = '│'
+vim.cmd('hi IndentBlanklineContextChar cterm=nocombine ctermfg=100 gui=nocombine guifg=#a8a8a8')
